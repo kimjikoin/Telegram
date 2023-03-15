8f0b1de322541e1c7ac5c1bdeadaa2b7d3c6f33e
Afzal0006 <121087528+Afzal0006@users.noreply.github.com>
2023-03-15 11:26:51 +0530

    Update start.py

--- AnonX/utils/inline/start.py
+++ AnonX/utils/inline/start.py
@@ -49,7 +49,7 @@ def private_panel(_, BOT_USERNAME, OWNER: Union[bool, int] = None):
         ],
         [
             InlineKeyboardButton(
-                text="🥂 sᴏᴜʀᴄᴇ 🥂", url=f"https://te.legra.ph/file/67d64056c060bed1ab18e.jpg",
+                text="🥂 sᴏᴜʀᴄᴇ 🥂", url=f"https://te.legra.ph/file/b073b5c337e7b260d49a5.mp4",
             )
         ],
      ]
