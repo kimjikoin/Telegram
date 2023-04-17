ac8e224f596364e1196f711ff0efcfd76b4521f4
kimjikoin <130832705+kimjikoin@users.noreply.github.com>
2023-04-17 12:23:42 +0530

    ʜᴀʀʀʏ ᴋᴏ ʙᴏʟᴜ ᴏʀ ʀᴇᴀʟ ᴀᴄᴄᴏᴜɴᴛ sᴇ ᴀʏᴇɢᴀ ..ᴀʜ?

--- AnonX/plugins/play.py
+++ AnonX/plugins/play.py
@@ -582,7 +582,7 @@ async def play_music(client, CallbackQuery, _):
 async def anonymous_check(client, CallbackQuery):
     try:
         await CallbackQuery.answer(
-            "ʏᴏᴜ'ʀᴇ ᴀɴ ᴀɴᴏɴʏᴍᴏᴜs ᴀᴅᴍɪɴ\n\nʀᴇᴠᴇʀᴛ ʙᴀᴄᴋ ᴛᴏ ᴜsᴇʀ ᴀᴄᴄᴏᴜɴᴛ ғᴏʀ ᴜsɪɴɢ ᴍᴇ.",
+            "ʏᴏᴜ'ʀᴇ ᴀɴ ᴀɴᴏɴʏᴍᴏᴜs ᴀᴅᴍɪɴ\n\nʜᴀʀʀʏ ᴋᴏ ʙᴏʟᴜ ᴏʀ ʀᴇᴀʟ ᴀᴄᴄᴏᴜɴᴛ sᴇ ᴀʏᴇɢᴀ ..ᴀʜ?.",
             show_alert=True,
         )
     except:
